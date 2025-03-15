aaecd0a37c10d73c5dd93e2c60bba6aec6e20937
Wesley Silva <desenvolvimentowesleyleopoldo@gmail.com>
2025-03-15 17:33:39 -0300

    Remove unused UUID import and simplify UserHelper class structure

--- src/main/java/com/getloc/syntech/getloc/helper/UserHelper.java
+++ src/main/java/com/getloc/syntech/getloc/helper/UserHelper.java
@@ -1,22 +1,9 @@
 package com.getloc.syntech.getloc.helper;
 
-import java.util.UUID;
+// import java.util.UUID;
 
-import com.getloc.syntech.getloc.utils.Utils;
+public class UserHelper{
 
-public class UserHelper extends Utils{
-
-    public UserHelper(UUID uuid) {
-        super();
-    }
-
-    public UserHelper setUuid(UUID newUuid) {
-        uuid = newUuid;
-        return this;
-    }
-    
-    public UserHelper createRandomUUID() {
-        uuid = UUID.randomUUID();
-        return this;
+    public UserHelper() {
     }
 }
