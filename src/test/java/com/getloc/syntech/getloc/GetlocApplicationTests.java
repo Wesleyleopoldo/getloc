ee550696f098903871a6491619f3501df3ba4608
Wesley Silva <desenvolvimentowesleyleopoldo@gmail.com>
2025-03-15 17:35:15 -0300

    Add unit tests for UUID binary generation and conversion methods

--- src/test/java/com/getloc/syntech/getloc/GetlocApplicationTests.java
+++ src/test/java/com/getloc/syntech/getloc/GetlocApplicationTests.java
@@ -1,13 +1,30 @@
 package com.getloc.syntech.getloc;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.util.UUID;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import com.getloc.syntech.getloc.utils.Utils;
 
 @SpringBootTest
 class GetlocApplicationTests {
 
 	@Test
-	void contextLoads() {
+	void testGenerateRandomBinaryByUUID(){
+		byte[] binaryCreated = Utils.create().generateRandomUUID().generateRandomBinaryByUUID();
+
+		assertTrue(binaryCreated instanceof byte[], "O valor retornado não é um byte[]" + binaryCreated);
+	}
+
+	@Test
+	void testConvertBytesForUUID(){
+		byte[] binaryCreated = Utils.create().generateRandomUUID().generateRandomBinaryByUUID();
+		UUID uuidCreated = Utils.create().convertBytesForUUID(binaryCreated);
+
+		assertTrue(uuidCreated instanceof UUID, "O valor retornado não é um UUID");
 	}
 
 }
