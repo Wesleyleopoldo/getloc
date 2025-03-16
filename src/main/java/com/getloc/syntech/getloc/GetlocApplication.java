7f482040b28a7c818befe1781241eb89d7e42c52
Wesley Silva <desenvolvimentowesleyleopoldo@gmail.com>
2025-03-16 01:41:10 -0300

    Reescreve start da aplicação springboot

--- src/main/java/com/getloc/syntech/getloc/GetlocApplication.java
+++ src/main/java/com/getloc/syntech/getloc/GetlocApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class GetlocApplication {
+public class GetlocApplication{
 
 	public static void main(String[] args) {
 		SpringApplication.run(GetlocApplication.class, args);
 	}
-
+	
 }
