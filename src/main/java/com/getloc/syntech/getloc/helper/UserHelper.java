7be9480c268c6edb5590eecee4f4e28cd33098d2
Wesley Silva <desenvolvimentowesleyleopoldo@gmail.com>
2025-03-16 01:44:27 -0300

    Adiciona anotação @Component na classe helper de usuário

--- src/main/java/com/getloc/syntech/getloc/helper/UserHelper.java
+++ src/main/java/com/getloc/syntech/getloc/helper/UserHelper.java
@@ -1,9 +1,18 @@
 package com.getloc.syntech.getloc.helper;
 
-// import java.util.UUID;
+import org.springframework.stereotype.Component;
 
+import com.getloc.syntech.getloc.admin.User;
+import com.getloc.syntech.getloc.responsesDTO.UserDTO;
+
+
+@Component
 public class UserHelper{
 
     public UserHelper() {
+    }
+
+    public UserDTO createUserDTO(User user) {
+        return new UserDTO(user.getUserId(), user.getName());
     }
 }
